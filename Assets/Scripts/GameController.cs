3fb6fb50409de6fe1431016ea09e49e8003ce4f8
Matsitsky Serhii <matsitskyss@gmail.com>
2022-09-03 23:35:17 +0300

    fix camera by player

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,6 +5,8 @@ public class GameController : MonoBehaviour
     public PlayerController player;
     protected Joystick joystick;
     protected Joybutton joybutton;
+    protected Camera mMainCamera;
+    protected Vector3 diffPlayerCamera;
     public EnemyManager enemyManager;
     private float XRange = 15;
     // Start is called before the first frame update
@@ -12,6 +14,9 @@ public class GameController : MonoBehaviour
     {
         joystick = FindObjectOfType<Joystick>();
         joybutton = FindObjectOfType<Joybutton>();
+        mMainCamera = Camera.main;
+        Vector3 cameraPosition = Camera.main.transform.position;
+        diffPlayerCamera = new Vector3(cameraPosition.x, cameraPosition.y, cameraPosition.z) - player.transform.position;
         enemyManager.startSpawn();
     }
 
@@ -27,7 +32,10 @@ public class GameController : MonoBehaviour
             player.transform.position = new Vector3(XRange, player.transform.position.y, player.transform.position.z);
         }
 
+        player.transform.Translate(Vector3.forward * joystick.Vertical * Time.deltaTime * player.speed);
         player.transform.Translate(Vector3.right * joystick.Horizontal * Time.deltaTime * player.speed);
+        mMainCamera.transform.position = player.transform.position + diffPlayerCamera;
+
 
         if (joybutton.Pressed)
         {
